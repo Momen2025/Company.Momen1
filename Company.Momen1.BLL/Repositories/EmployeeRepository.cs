a1e30a59576c538e05bcf10eccfa2710094fbdba
momenhabib88@gmail.com <momenhabib88@gmail.com>
2025-03-18 23:24:27 +0200

    Implenmentation of BLL  for  Employee Seveice Before GEnaric

---
+++ Company.Momen1.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using Company.Momen1.BLL.Interfaces;
+using Company.Momen1.DAL.Data.Contexts;
+using Company.Momen1.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Momen1.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepository(CompanyDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee? Get(int Id)
+        {
+            return _context.Employees.Find(Id);
+
+        }
+
+        public int Add(Employee model)
+        {
+            _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+        public int Update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+   
+    }
+}
